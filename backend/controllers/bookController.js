514ccd17e07f22095d21644db9a1fbcf2ab567a4
Jayden-cuu <jayden.cuu@gmail.com>
2026-03-27 19:40:32 +1000

    Create bookController.js

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,52 @@
+const Book = require('../models/book');
+
+const getBooks = async (req, res) => {
+    try{
+        const books = await Book.find();
+        res.json(books);
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+const addBook = async (req,res) => {
+    const {title, author, price, type} = req.body;
+    try{
+        const book = await Book.create({title, author, price, type});
+        res.status(201).json(book);
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+const updateBook = async (req,res) => {
+    const {title, author, price, type} = req.body;
+    try{
+        const book = await Book.findById(req.params.id);
+        if (!book) return res.status(404).json({message:'Book not found'});
+
+        book.title = title || book.title;
+        book.author = author || book.author;
+        book.price = price || book.price;
+        book.type = type || book.type;
+
+        const updatedBook = await book.save();
+        res.json(updatedBook);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
+const deleteBook = async (req,res) => {
+    try{
+        const book = await Book.findById(req.params.id);
+        if (!book) return res.status(404).json({message: 'Book not found'});
+
+        await book.deleteOne();
+        res.json({message:'Book deleted'});
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+module.exports = {getBooks, addBook, updateBook, deleteBook};
