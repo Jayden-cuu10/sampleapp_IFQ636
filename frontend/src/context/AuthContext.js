6ebcab62b9d19b56152d3b18fd8b287f4a147d62
Jayden-cuu <jayden.cuu@gmail.com>
2026-04-01 14:23:48 +1000

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,16 +1,26 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  useEffect(()=>{
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  },[]);
+
   const login = (userData) => {
     setUser(userData);
+    localStorage.setItem('user',JSON.stringify(useData));
   };
 
   const logout = () => {
     setUser(null);
+    localStorage.removeItem('user');
   };
 
   return (
